de925397757eeb4c7ee53ef5183cbc864b7422e9
Ziyaad <xavierbaatjes@yahoo.com>
2024-03-23 17:30:24 +0200

    Update from_tortoise.py

--- from_tortoise.py
+++ from_tortoise.py
@@ -9,8 +9,7 @@ cfg = json.load(open(cfg_path))
 logs_folder = Path(cfg['gpt_train']['logs_dir'])
 logs_folder.mkdir(exist_ok=True, parents=True)
 
-checkpoint = load_fsspec("C:/Users/User/PycharmProjects/new_tortoise/.models/autoregressive.pth",
-                         map_location=torch.device("cpu"))
+checkpoint = load_fsspec("original_models/autoregressive.pth", map_location=torch.device("cpu"))
 
 data = {
     'step': 0,
