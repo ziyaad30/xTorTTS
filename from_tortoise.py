3ea81abc8b7fd72b43b5bd57bb0489eee5b6173e
Ziyaad <xavierbaatjes@yahoo.com>
2024-03-23 17:29:11 +0200

    Add files via upload

---
+++ from_tortoise.py
@@ -0,0 +1,21 @@
+import json
+from pathlib import Path
+import torch
+from utils.io import load_fsspec
+
+
+cfg_path = 'configs/config.json'
+cfg = json.load(open(cfg_path))
+logs_folder = Path(cfg['gpt_train']['logs_dir'])
+logs_folder.mkdir(exist_ok=True, parents=True)
+
+checkpoint = load_fsspec("C:/Users/User/PycharmProjects/new_tortoise/.models/autoregressive.pth",
+                         map_location=torch.device("cpu"))
+
+data = {
+    'step': 0,
+    'epoch': 0,
+    'model': checkpoint,
+}
+torch.save(data, f'{logs_folder}/GPTT_0.pth')
+
