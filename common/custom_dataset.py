7475dfdeb7f4d446415c7da23ea497acafe6bd5d
Ziyaad <xavierbaatjes@yahoo.com>
2024-03-24 20:46:11 +0200

    Update custom_dataset.py

--- common/custom_dataset.py
+++ common/custom_dataset.py
@@ -8,7 +8,7 @@ import torchaudio
 from torch import LongTensor
 from torch.utils.data import Dataset
 
-from audio.injectors import TortoiseDiscreteTokenInjector, TorchMelSpectrogramInjector, TorchMelSpectrogramInjector_Base
+from audio.injectors import TortoiseDiscreteTokenInjector, TortoiseMelSpectrogramInjector
 
 
 def parse_filelist(filelist_path, split_char="|"):
@@ -17,8 +17,35 @@ def parse_filelist(filelist_path, split_char="|"):
     return filepaths_and_text
 
 
+def get_prompt_slice(audio, max_audio_length=20, min_audio_length=3, sample_rate=24000, is_eval=False):
+    max_sample_length = max_audio_length * sample_rate
+    min_sample_length = min_audio_length * sample_rate
+    rel_clip = audio
+    # if eval uses a middle size sample when it is possible to be more reproducible
+    if is_eval:
+        sample_length = int((min_sample_length + max_sample_length) / 2)
+    else:
+        sample_length = random.randint(min_sample_length, max_sample_length)
+    gap = rel_clip.shape[-1] - sample_length
+    if gap < 0 and is_eval:
+        sample_length = rel_clip.shape[-1]
+    elif gap < 0:
+        sample_length = rel_clip.shape[-1] // 2
+    gap = rel_clip.shape[-1] - sample_length
+
+    # if eval start always from the position 0 to be more reproducible
+    if is_eval:
+        rand_start = 0
+    else:
+        rand_start = random.randint(0, gap)
+
+    rand_end = rand_start + sample_length
+    rel_clip = rel_clip[:, rand_start:rand_end]
+    return rel_clip
+
+
 class GptDataset(Dataset):
-    def __init__(self, config, tokenizer, dvae_path, mel_path, mel_norm_file=None, is_eval=False):
+    def __init__(self, config, tokenizer, dvae_path, injector, is_eval=False):
         self.is_eval = is_eval
         if not self.is_eval:
             self.path = config['gpt_train']['train_file']
@@ -26,63 +53,71 @@ class GptDataset(Dataset):
             self.path = config['gpt_train']['valid_file']
         self.tokenizer = tokenizer
         self.audiopath_and_text = parse_filelist(self.path)
+        self.sample_rate = config['vae_train']['sample_rate']
+        self.n_mels = config['vae_train']['n_mels']
+        self.power = config['vae_train']['power']
 
-        self.mel_norm_file = mel_norm_file
-        self.inj = TorchMelSpectrogramInjector({'in': 'wav', 'out': 'mel'}, mel_norm_file=self.mel_norm_file)
-        self.mel_path = mel_path
+        try:
+            self.mel_fmax = config['vae_train']['mel_fmax']
+        except:
+            self.mel_fmax = None
+
+        self.inj = injector
+        self.mel_path = config['gpt_train']['mel_dir']
         self.dvae_path = dvae_path
-        self.code_inj = TortoiseDiscreteTokenInjector({'in': 'mel', 'out': 'codes'}, self.dvae_path)
+        self.code_inj = TortoiseDiscreteTokenInjector({'in': 'mel', 'out': 'codes'}, self.dvae_path,
+                                                      channels=self.n_mels)
 
     def get_text(self, text):
         tokens = self.tokenizer.encode(text)
         tokens = torch.IntTensor(tokens)
-        assert not torch.any(tokens == 1), f"UNK token found in {text} -> {self.tokenizer.decode(tokens)}"
-        # The stop token should always be sacred.
-        assert not torch.any(tokens == 0), f"Stop token found in {text}"
         return tokens
 
     def __getitem__(self, index):
-        try:
-            audiopath_and_text = self.audiopath_and_text[index]
-            wav_file, text = audiopath_and_text[0], audiopath_and_text[1]
+        audiopath_and_text = self.audiopath_and_text[index]
+        wav_file, text = audiopath_and_text[0], audiopath_and_text[1]
 
-            tseq = self.get_text(text)
+        tseq = self.get_text(text)
 
-            audio, sr = torchaudio.load(wav_file)
+        # The stop token should always be sacred.
+        if torch.any(tseq == 0):
+            raise Exception(f"Stop token found in {text}")
 
-            if audio.shape[0] > 1:
-                audio = audio[0].unsqueeze(0)
-            audio = torchaudio.transforms.Resample(sr, 22050)(audio).cuda()
+        if torch.any(tseq == 1):
+            raise Exception(f"[UNK] token found in ===> {text} -> {self.tokenizer.decode(tseq)}")
 
-            base_name = Path(wav_file).stem
+        audio, sr = torchaudio.load(wav_file)
 
-            if not os.path.exists(f'{self.mel_path}/{base_name}.mel.pth'):
-                mel = self.inj({'wav': audio.unsqueeze(0)})['mel']
-                torch.save(mel.cpu().detach(), f'{self.mel_path}/{base_name}.mel.pth')
+        if audio.shape[0] > 1:
+            audio = audio[0].unsqueeze(0)
+        audio = torchaudio.transforms.Resample(sr, self.sample_rate)(audio).cuda()
 
-            mel = torch.load(f'{self.mel_path}/{base_name}.mel.pth')
+        """base_name = Path(wav_file).stem
 
-            if not os.path.exists(f'{self.mel_path}/{base_name}.melvq.pth'):
-                # print(base_name)
-                code = self.code_inj({'mel': mel.to('cuda')})['codes']
-                torch.save(code, f'{self.mel_path}/{base_name}.melvq.pth')
+        if not os.path.exists(f'{self.mel_path}/{base_name}.mel.pth'):
+            mel = self.inj({'wav': audio.unsqueeze(0)})['mel']
+            torch.save(mel.cpu().detach(), f'{self.mel_path}/{base_name}.mel.pth')
 
-            qmel = torch.load(f'{self.mel_path}/{base_name}.melvq.pth')
+        mel = torch.load(f'{self.mel_path}/{base_name}.mel.pth')
 
-            mel = mel[0]
-            qmel = qmel[0]
-            wav_length = mel.shape[1] * 256
-
+        if not os.path.exists(f'{self.mel_path}/{base_name}.melvq.pth'):
             # print(base_name)
-            # print(qmel.shape[0])
+            code = self.code_inj({'mel': mel.to('cuda')})['codes']
+            torch.save(code, f'{self.mel_path}/{base_name}.melvq.pth')
 
-            split = random.randint(int(mel.shape[1] // 3), int(mel.shape[1] // 3 * 2))
-            if random.random() > 0.5:
-                cond_mel = mel[:, :split]
-            else:
-                cond_mel = mel[:, split:]
-        except:
-            return None
+        qmel = torch.load(f'{self.mel_path}/{base_name}.melvq.pth')"""
+
+        mel = self.inj({'wav': audio.unsqueeze(0)})['mel']
+        qmel = self.code_inj({'mel': mel.to('cuda')})['codes']
+        mel = mel[0]
+        qmel = qmel[0]
+        wav_length = mel.shape[1] * 256
+
+        split = random.randint(int(mel.shape[1] // 3), int(mel.shape[1] // 3 * 2))
+        if random.random() > 0.5:
+            cond_mel = mel[:, :split]
+        else:
+            cond_mel = mel[:, split:]
 
         if tseq.shape[0] > 400 or qmel.shape[0] > 600:
             print(f"Warning: {text} text len {tseq.shape[0]} exceed 400 , qmel len {qmel.shape[0]} exceed 600.")
@@ -155,14 +190,24 @@ class GptDataset(Dataset):
 
 class DvaeMelDataset(torch.utils.data.Dataset):
     def __init__(self, config):
+        print(config['vae_train'])
         self.path = config['gpt_train']['train_file']
         self.sample_rate = config['vae_train']['sample_rate']
         self.n_mels = config['vae_train']['n_mels']
 
+        try:
+            self.mel_fmax = config['vae_train']['mel_fmax']
+        except:
+            self.mel_fmax = None
+
         self.pad_to = config['vae_train']['pad_to_samples']
         self.squeeze = config['vae_train']['squeeze']
-        self.inj = TorchMelSpectrogramInjector_Base({'in': 'wav', 'out': 'mel'})
+        self.inj = TortoiseMelSpectrogramInjector({'in': 'wav', 'out': 'mel'},
+                                                  n_mel_channels=self.n_mels,
+                                                  sampling_rate=self.sample_rate,
+                                                  mel_fmax=self.mel_fmax)
         self.audiopath_and_text = parse_filelist(self.path)
+        self.mel_path = config['gpt_train']['mel_dir']
 
     def __getitem__(self, index):
         audiopath_and_text = self.audiopath_and_text[index]
@@ -174,7 +219,14 @@ class DvaeMelDataset(torch.utils.data.Dataset):
             audio = audio[0].unsqueeze(0)
         audio = torchaudio.transforms.Resample(sr, self.sample_rate)(audio).cuda()
 
-        mel = self.inj({'wav': audio.unsqueeze(0)})['mel']
+        base_name = Path(wav_file).stem
+
+        if not os.path.exists(f'{self.mel_path}/{base_name}.mel.pth'):
+            print(base_name)
+            mel = self.inj({'wav': audio.unsqueeze(0)})['mel']
+            torch.save(mel.cpu().detach(), f'{self.mel_path}/{base_name}.mel.pth')
+
+        mel = torch.load(f'{self.mel_path}/{base_name}.mel.pth')
 
         if mel.shape[-1] >= self.pad_to:
             start = torch.randint(0, mel.shape[-1] - self.pad_to + 1, (1,))
